0a53aab356ba012392dc537bb1ad984c723a74d3
Đỗ Duy Thành <84120300+duythanh22@users.noreply.github.com>
2023-09-27 11:16:27 +0700

    Add files via upload

---
+++ A_star/alogs.py
@@ -0,0 +1,242 @@
+import sys
+from heapq import heappush, heappop
+from time import sleep
+import threading
+from multiprocessing import Queue
+
+
+MAP_DICT = {
+    "STARTING_POINT": 'A',
+    "ENDING_POINT": 'B',
+    "WATER": 'W',
+    "PAVED_ROAD": 'R'
+}
+
+COST_DICT = {
+    "A": 0,
+    "B": 0,
+    "R": 1
+}
+
+PATH_DICT = {
+    "UP": 'U',
+    "DOWN": 'D',
+    "LEFT": 'L',
+    "RIGHT": 'R'
+}
+
+
+def find_start_and_end(grid):
+    start, end = None, None
+    for j, row in enumerate(grid):
+        for i, cell in enumerate(row):
+            if cell == MAP_DICT["STARTING_POINT"]:
+                start = (i, j)
+            elif cell == MAP_DICT["ENDING_POINT"]:
+                end = (i, j)
+
+    return start, end
+
+
+def get_manhattan_distance(pos, goal):
+    return abs(pos[0] - goal[0]) + abs(pos[1] - goal[1])
+
+
+def get_successors(node, grid, size):
+    successors = []
+    x, y = node.x, node.y
+
+    if (x + 1 < size) and (grid[y][x + 1] != MAP_DICT["WATER"]):
+        successors.append((x + 1, y))
+
+    if (x - 1 >= 0) and (grid[y][x - 1] != MAP_DICT["WATER"]):
+        successors.append((x - 1, y))
+
+    if (y + 1 < size) and (grid[y + 1][x] != MAP_DICT["WATER"]):
+        successors.append((x, y + 1))
+
+    if (y - 1 >= 0) and (grid[y - 1][x] != MAP_DICT["WATER"]):
+        successors.append((x, y - 1))
+
+    return successors
+
+
+def get_path(node, grid):
+    path = []
+    cost = 0
+    while node is not None:
+        path.append(node)
+        node = node.parent
+
+    path = path[::-1]
+    dir_path = []
+    for i in range(len(path)-1):
+        cost += COST_DICT[grid[path[i].y][path[i].x]]
+
+        new_x = path[i+1].x - path[i].x
+        new_y = path[i+1].y - path[i].y
+        if new_x == 1:
+            dir_path.append(PATH_DICT["RIGHT"])
+        elif new_x == -1:
+            dir_path.append(PATH_DICT["LEFT"])
+        elif new_y == 1:
+            dir_path.append(PATH_DICT["DOWN"])
+        elif new_y == -1:
+            dir_path.append(PATH_DICT["UP"])
+
+    return '-'.join(dir_path), cost
+
+
+class Node:
+    def __init__(self, x, y, h, cost=0, parent=None):
+        self.x = x
+        self.y = y
+        self.h = h
+        self.g = cost + parent.g if parent else 0
+        self.f = h+self.g
+        self.parent = parent
+
+    def __lt__(self, other):
+        return self.f < other.f
+
+
+class GreedyBestFirstSearch:
+    def __init__(self, grid, size):
+        self.grid = [row[:] for row in grid]  # deep copy
+        self.size = size
+
+        self.start, self.end = find_start_and_end(grid)
+
+    def get_successors(self, node):
+        return get_successors(node, self.grid, self.size)
+
+    def get_heuristic(self, pos):
+        return get_manhattan_distance(pos, self.end)
+
+    def get_path_cost(self, node):
+        cost = 0
+        while node is not None:
+            cost += COST_DICT[self.grid[node.y][node.x]]
+            node = node.parent
+
+        return cost
+
+    def run(self, q=None):
+        opened = []
+        closed = []
+
+        h = self.get_heuristic(self.start)
+        heappush(opened, Node(self.start[0], self.start[1], h))
+
+        while len(opened) > 0:
+            # set current node
+            node = heappop(opened)
+            closed.append(node)
+
+            # if the current node is the end node, return the path
+            if node.x == self.end[0] and node.y == self.end[1]:
+                q.put(("end", node, self.get_path_cost(node)))
+                return
+
+            # get successors
+            successors = self.get_successors(node)
+
+            if q is not None:
+                q.put((node, successors))
+
+            for successor in successors:
+                inOpened = False
+                inClosed = False
+
+                # check if in opened
+                for i in range(len(opened)):
+                    if opened[i].x == successor[0] and opened[i].y == successor[1]:
+                        inOpened = True
+                        break
+
+                # check if in closed
+                for i in range(len(closed)):
+                    if closed[i].x == successor[0] and closed[i].y == successor[1]:
+                        inClosed = True
+                        break
+
+                if not inOpened and not inClosed:
+                    heappush(opened, Node(successor[0], successor[1],
+                                          self.get_heuristic(successor), parent=node))
+
+        return False
+
+
+class AStar:
+    def __init__(self, grid, size):
+        self.grid = [row[:] for row in grid]  # deep copy
+        self.size = size
+        self.start, self.end = find_start_and_end(grid)
+
+    def get_successors(self, node):
+        return get_successors(node, self.grid, self.size)
+
+    def get_heuristic(self, pos):
+        return get_manhattan_distance(pos, self.end)
+
+    def run(self, q=None):
+        opened = []
+        closed = []
+
+        h = self.get_heuristic(self.start)
+        node = Node(self.start[0], self.start[1], h)
+        heappush(opened, node)
+
+        while len(opened) > 0:
+            # set current node
+            node = heappop(opened)
+            closed.append(node)
+
+            # if the current node is the end node, return the path
+            if node.x == self.end[0] and node.y == self.end[1]:
+                q.put(("end", node, node.g))
+                return True
+
+            # get successors
+            successors = self.get_successors(node)
+
+            if q is not None:
+                q.put((node, successors))
+
+            for successor in successors:
+                h = self.get_heuristic(successor)
+                cost = COST_DICT[self.grid[successor[1]][successor[0]]]
+                successor = Node(successor[0], successor[1], h, cost, node)
+
+                inOpened = False
+                inClosed = False
+
+                for i in range(len(opened)):
+                    if opened[i].x == successor.x and opened[i].y == successor.y:
+                        inOpened = True
+                        if opened[i].f > successor.f:
+                            opened[i] = successor
+                            break
+
+                for i in range(len(closed)):
+                    if closed[i].x == successor.x and closed[i].y == successor.y:
+                        inClosed = True
+                        if closed[i].f > successor.f:
+                            closed[i] = successor
+                            break
+
+                if not inOpened and not inClosed:
+                    heappush(opened, successor)
+
+        return False
+
+
+def read_file(file_name):
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        size, input_mat = lines[0].strip(), lines[1:]
+
+    size = int(size)
+    grid = [[letter for letter in row.strip()] for row in input_mat]
+
+    return size, grid
---
+++ A_star/gui.py
@@ -0,0 +1,271 @@
+import tkinter as tk
+from tkinter import messagebox
+import alogs
+from alogs import AStar, GreedyBestFirstSearch, MAP_DICT, Node
+import os
+import time
+from threading import Thread
+from multiprocessing import Queue
+
+COLOR_DICT = {
+    'R': 'Yellow',  # paved road
+    'W': 'Blue',  # water
+    'A': 'Green',  # start
+    'B': 'Red',  # end
+    'V': 'Black',  # visited
+    'S': 'White',  # successors
+    'P': 'Magenta',  # path
+}
+
+VISU_DICT = {
+    "VISITED": 'V',
+    "SUCCESSOR": 'S',
+    "PATH": 'P',
+}
+
+COLOR_ID = ['R', 'W', 'A', 'B']  # order of cycling clicks
+
+class Frame():
+    num_of_rows = 0
+    num_of_cols = 0
+    last_dragged = None
+
+    def __init__(self, root, *args, **kwargs):
+        self.root = root
+
+        self.menu = tk.Menu(root)
+        self.menu.add_command(label="Load", command=self.load)
+        self.menu.add_command(label="Save", command=self.save)
+        self.menu.add_command(label="Reset", command=self.reset)
+        self.menu.add_command(label="Run", command=self.run)
+        root.config(menu=self.menu)
+
+        self.grid = []  # actual grid
+        self.display_grid = []  # grid to display on canvas
+
+        self.queue = Queue()
+        self.visu_speed = 4 # higher = faster. deteremines how fast the visualization runs
+        self.is_dirty = False  # is the grid dirty (path is shown)
+
+        # Setup Canvas
+        self.c = tk.Canvas(root, height=600, width=600, bg='white')
+        self.c.pack(fill=tk.BOTH, expand=True)
+        self.c.bind('<Configure>', self.init_grid)
+        self.pixel_width = 20
+        self.pixel_height = 20
+
+        self.cost = None
+
+        self.statusbar = tk.Label(
+            root, text="", bd=1, relief=tk.SUNKEN, anchor=tk.W)
+        self.statusbar.pack(side=tk.BOTTOM, fill=tk.X)
+        self.update_status_bar()
+
+        # Setup binds
+        self.c.bind("<ButtonPress-1>", self.left_click)
+        self.c.bind("<ButtonPress-3>", self.right_click)
+        self.c.bind("<B1-Motion>", self.left_click_drag)
+
+        self.root.after(0, self.refresh_grid)
+
+    def update_status_bar(self):
+        value = ""
+        value += f"\tCost: {self.cost}" if self.cost else ""
+        self.statusbar.config(text=value)
+
+    def get_pos_in_grid(self, item):
+        item = item[0] - 1
+        x = int(item % self.num_of_cols)
+        y = int(item / self.num_of_cols)
+        return x, y
+
+    def update_cell(self, items, drag=False):
+        x, y = self.get_pos_in_grid(items)
+
+        if drag:
+            if self.last_dragged == (x, y):
+                return
+        self.last_dragged = (x, y)
+
+        next_tile = COLOR_ID[(COLOR_ID.index(
+            self.grid[y][x]) + 1) % len(COLOR_ID)]
+        next_color = COLOR_DICT[next_tile]
+        self.grid[y][x] = next_tile
+        self.display_grid[y][x] = next_tile
+
+        rect_id = items[0]
+        self.c.itemconfigure(rect_id, fill=next_color)
+
+    def right_click(self, event):
+        if self.is_dirty:
+            self.reset_display_grid()
+            self.color_grid()
+            self.is_dirty = False
+
+    def left_click(self, event):
+        if self.is_dirty:
+            self.reset_display_grid()
+            self.color_grid()
+            self.is_dirty = False
+
+        items = self.c.find_closest(event.x, event.y)
+        if items:
+            self.update_cell(items)
+
+    def left_click_drag(self, event):
+        if self.is_dirty:
+            self.reset_display_grid()
+            self.color_grid()
+            self.is_dirty = False
+
+        items = self.c.find_closest(event.x, event.y)
+        if items:
+            self.update_cell(items, True)
+
+    def display_path(self, node):
+        path = []
+        while node is not None:
+            path.append(node)
+            node = node.parent
+
+        path = path[::-1]
+
+        for node in path:
+            if self.grid[node.y][node.x] == MAP_DICT["ENDING_POINT"]:
+                self.display_grid[node.y][node.x] = MAP_DICT["ENDING_POINT"]
+            elif self.grid[node.y][node.x] == MAP_DICT["STARTING_POINT"]:
+                self.display_grid[node.y][node.x] = MAP_DICT["STARTING_POINT"]
+            else:
+                self.display_grid[node.y][node.x] = VISU_DICT["PATH"]
+
+
+    def refresh_grid(self):
+        '''Checks if there are any new items to be added to the canvas,
+         and colors the grid if so'''
+        items = []
+
+        for _ in range(self.visu_speed):
+            try:
+                items.append(self.queue.get_nowait())
+            except:
+                pass
+
+        for item in items:
+            if item[0] == "end":
+                node, cost = item[1], item[2]
+                self.display_path(node)
+                self.cost = cost
+                self.update_status_bar()
+            else:
+                node, successors = item
+                self.update_grid_by_algo_run(successors)
+
+        if len(items) > 0:
+            self.color_grid()
+
+        self.root.after(10, self.refresh_grid)
+
+    def color_grid(self):
+        '''Color the grid according to display_grid'''
+        num_of_items = self.num_of_rows * self.num_of_cols
+        for i in range(num_of_items):
+            x = int(i % self.num_of_cols)
+            y = int(i / self.num_of_cols)
+            self.c.itemconfigure(i+1, fill=COLOR_DICT[self.display_grid[y][x]])
+
+    def create_grid(self):
+        for y in range(self.num_of_rows):
+            for x in range(self.num_of_cols):
+                x1 = (x * self.pixel_width)
+                x2 = (x1 + self.pixel_width)
+                y1 = (y * self.pixel_height)
+                y2 = (y1 + self.pixel_height)
+                self.c.create_rectangle(
+                    x1, y1, x2, y2, fill=COLOR_DICT[self.grid[y][x]])
+        self.c.update()
+
+    def init_grid(self, event=None):
+        '''Initialize the grid with yellow paved roads'''
+        self.num_of_rows = int(self.c.winfo_height() / self.pixel_height)
+        self.num_of_cols = int(self.c.winfo_width() / self.pixel_width)
+        print("Rows: " + str(self.num_of_rows))
+        print("Cols: " + str(self.num_of_cols))
+        self.grid = [[MAP_DICT["PAVED_ROAD"] for _ in range(
+            self.num_of_cols)] for _ in range(self.num_of_rows)]
+
+        self.display_grid = [row[:] for row in self.grid]
+
+        self.create_grid()
+
+    def reset_display_grid(self):
+        self.display_grid = [row[:] for row in self.grid]
+
+    def load(self):
+        size, self.grid = alogs.read_file("input.txt")
+        self.display_grid = [row[:] for row in self.grid]
+        self.num_of_rows = size
+        self.num_of_cols = size
+        self.color_grid()
+        self.cost = None
+        self.update_status_bar()
+
+    def save(self):
+        with(open("input.txt", "w+")) as f:
+            f.write(str(self.num_of_rows) + "\n")
+
+            for row in self.grid:
+                f.write("".join(row) + "\n")
+            # remove last charachter
+            f.truncate(f.tell()-len(os.linesep))
+
+    def run(self):
+        if alogs.find_start_and_end(self.grid)[0] is None or alogs.find_start_and_end(self.grid)[1] is None:
+            tk.messagebox.showerror(
+                "Error", "Missing starting and/or ending point(s)")
+            return
+
+        self.is_dirty = True
+
+        self.cost = None
+        self.update_status_bar()
+        self.reset_display_grid()
+        self.color_grid()
+
+        algo = None
+
+        algo = AStar(self.grid, self.num_of_rows)
+
+        algo_thread = Thread(target=algo.run, args=(self.queue,))
+        algo_thread.start()
+
+    def reset(self):
+        for y in range(self.num_of_rows):
+            for x in range(self.num_of_cols):
+                self.grid[y][x] = MAP_DICT["PAVED_ROAD"]
+                self.display_grid[y][x] = MAP_DICT["PAVED_ROAD"]
+        self.color_grid()
+
+        self.cost = None
+        self.update_status_bar()
+
+    def update_grid_by_algo_run(self, successors):
+        # set node color
+        # self.grid[node[1]][node[0]] = VISU_DICT["VISITED"]
+
+        successors = [
+            s for s in successors]
+
+        for s in successors:
+            self.display_grid[s[1]][s[0]] = VISU_DICT["SUCCESSOR"]
+
+
+def main():
+    root = tk.Tk()
+    root.resizable(False, False)
+    root.eval('tk::PlaceWindow . center')
+    gui = Frame(root)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
